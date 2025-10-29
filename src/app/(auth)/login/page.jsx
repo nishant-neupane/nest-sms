d36a80e3449b2bbb4c05a43ec0f18e88bcfcb4ef
nishant-neupane <nishantneupane488@gmail.com>
2025-10-29 14:13:15 +0545

    minor cganges

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -4,12 +4,9 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
-import AuthLayout from "@/components/auth/AuthLayout";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Eye, EyeOff } from "lucide-react";
-
-// Use environment variable for API URL
 
 export default function LoginPage() {
   const router = useRouter();
@@ -31,167 +28,161 @@ export default function LoginPage() {
     try {
       const res = await fetch(`/api/auth/login`, {
         method: "POST",
+        credentials: "include",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
-        credentials: "include",
       });
 
-      if (res.ok) {
-        const data = await res.json();
-        toast.success("Login successful!");
+      const data = await res.json();
 
-        console.log("Cookies after login:", document.cookie);
-
-        setTimeout(() => {
-          router.push("/dashboard");
-        }, 1500);
-      } else {
-        const data = await res.json();
-        toast.error(
-          data.error || data.message || "Invalid credentials. Try again."
-        );
+      if (!res.ok) {
+        toast.error(data.error || "Invalid credentials");
+        return;
       }
+
+      toast.success("Login Successful!");
+      setTimeout(() => router.push("/dashboard"), 1500);
     } catch (error) {
-      console.error("Login error:", error);
-      toast.error("Network error. Please try again later.");
+      console.error(error);
+      toast.error("Network error. Try again later.");
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <AuthLayout>
-      <div className="bg-white rounded-2xl p-6">
-        <div className="flex justify-between items-center mb-6">
-          <Image
-            src="/images/auth/nest-logo.png"
-            alt="Nest Logo"
-            width={190}
-            height={50}
-            className="object-contain"
-          />
-          <p className="font-[500] text-base leading-6 text-black">
-            Don't have an account?{" "}
-            <Link href="/signup" className="text-[#2375E0] font-bold">
-              Sign Up
-            </Link>
-          </p>
-        </div>
-
-        <div className="px-8 pt-8">
-          <h2 className="font-[700] text-[40px] text-black mb-4">
-            Login to your account
-          </h2>
-          <p className="font-medium text-lg leading-6 text-black/70 mb-6">
-            Please enter your information to access your account.
-          </p>
-
-          <div className="grid grid-cols-2 gap-4 mb-6">
-            <button
-              type="button"
-              className="w-full flex items-center justify-center border border-[#A6A6A6] rounded-lg py-2.5 hover:scale-[1.03] transition-all duration-300 cursor-pointer font-medium text-base leading-6 text-black"
-            >
+    <div className="bg-[#E9EFF7] p-6 ">
+      <div className=" grid grid-cols-1 md:grid-cols-5  gap-8  justify-center items-center overflow-hidden">
+        <div className="p-8 h-full bg-white rounded-xl md:col-span-3 flex flex-col justify-center ">
+          <div className="w-full max-w-3xl mx-auto">
+            <div className="flex justify-between items-center mb-6">
               <Image
-                src="/images/auth/google.png"
-                alt="Google"
-                width={20}
-                height={20}
-                className="mr-2"
+                src="/images/auth/nest-logo.png"
+                alt="Nest Logo"
+                width={190}
+                height={50}
+                className="object-contain"
               />
-              Log in with Google
-            </button>
-            <button
-              type="button"
-              className="w-full flex items-center justify-center border border-[#A6A6A6] rounded-lg py-2.5 hover:scale-[1.03] transition-all duration-300 cursor-pointer font-medium text-base leading-6 text-black"
-            >
-              <Image
-                src="/images/auth/facebook.png"
-                alt="Facebook"
-                width={20}
-                height={20}
-                className="mr-2"
-              />
-              Log in with Facebook
-            </button>
-          </div>
-
-          <div className="flex items-center my-6">
-            <div className="flex-grow h-px bg-[#A6A6A6]"></div>
-            <span className="px-2 text-sm text-gray-500">or</span>
-            <div className="flex-grow h-px bg-[#A6A6A6]"></div>
-          </div>
-
-          <form onSubmit={handleSubmit} className="space-y-6">
-            <div className="flex flex-col gap-2">
-              <label
-                htmlFor="email"
-                className="font-medium text-lg leading-5 text-black"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Enter your email"
-                value={formData.email}
-                onChange={handleChange}
-                required
-                className="w-full border border-[#A6A6A6] rounded-lg p-2.5 focus:outline-none focus:ring-2 focus:ring-[#2375E0] font-medium text-base leading-6 text-[#A6A6A6]"
-              />
-            </div>
-
-            <div className="flex flex-col gap-2 relative">
-              <label
-                htmlFor="password"
-                className="font-medium text-lg leading-5 text-black"
-              >
-                Password
-              </label>
-              <input
-                type={showPassword ? "text" : "password"}
-                id="password"
-                name="password"
-                placeholder="Enter your password"
-                value={formData.password}
-                onChange={handleChange}
-                required
-                className="w-full border border-[#A6A6A6] rounded-lg p-2.5 focus:outline-none focus:ring-2 focus:ring-[#2375E0] font-medium text-base leading-6 text-[#A6A6A6]"
-              />
-              <button
-                type="button"
-                onClick={() => setShowPassword(!showPassword)}
-                className="absolute right-3 top-10 text-gray-400 cursor-pointer"
-              >
-                {showPassword ? <Eye size={20} /> : <EyeOff size={20} />}
-              </button>
-
-              <div className="text-right mt-1">
-                <Link
-                  href="/forgot-password"
-                  className="text-[#2375E0] font-medium text-base leading-6 hover:underline"
-                >
-                  Forgot password?
+              <p className="font-medium text-base text-black">
+                Don't have an account?{" "}
+                <Link href="/signup" className="text-[#2375E0] font-bold">
+                  Sign Up
                 </Link>
-              </div>
+              </p>
             </div>
+            <div className="px-8 ">
+              <h2 className="font-bold text-[40px] text-black mb-2">
+                Login to your account
+              </h2>
+              <p className="font-medium text-lg text-black/70 mb-2">
+                Please enter your information to access your account.
+              </p>
 
-            <button
-              type="submit"
-              disabled={loading}
-              className="w-full bg-[#2375E0] text-white py-2.5 rounded-lg font-semibold hover:scale-[1.03] transition-all duration-300 cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
-            >
-              {loading ? "Logging in..." : "Login"}
-            </button>
-          </form>
+              <div className="grid grid-cols-2 gap-4 mb-6">
+                <button className="w-full flex items-center justify-center border border-[#A6A6A6] rounded-lg py-2.5 hover:scale-[1.03] duration-300">
+                  <Image
+                    src="/images/auth/google.png"
+                    width={20}
+                    height={20}
+                    alt="Google"
+                    className="mr-2"
+                  />
+                  Log in with Google
+                </button>
 
-          <p className="text-center text-sm text-[#A6A6A6] mt-8">
-            © 2025. All rights reserved.
-          </p>
+                <button className="w-full flex items-center justify-center border border-[#A6A6A6] rounded-lg py-2.5 hover:scale-[1.03] duration-300">
+                  <Image
+                    src="/images/auth/facebook.png"
+                    width={20}
+                    height={20}
+                    alt="Facebook"
+                    className="mr-2"
+                  />
+                  Log in with Facebook
+                </button>
+              </div>
+
+              <div className="flex items-center my-2">
+                <div className="flex-grow h-px bg-[#A6A6A6]"></div>
+                <span className="px-2 text-sm text-gray-500">or</span>
+                <div className="flex-grow h-px bg-[#A6A6A6]"></div>
+              </div>
+
+              <form onSubmit={handleSubmit} className="space-y-2">
+                <div className="flex flex-col gap-2">
+                  <label className="font-medium text-lg text-black">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Enter your email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    required
+                    className="w-full border border-[#A6A6A6] rounded-lg p-2.5 focus:ring-2 focus:ring-[#2375E0]"
+                  />
+                </div>
+
+                <div className="flex flex-col gap-2 relative">
+                  <label className="font-medium text-lg text-black">
+                    Password
+                  </label>
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    name="password"
+                    placeholder="Enter your password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    required
+                    className="w-full border border-[#A6A6A6] rounded-lg p-2.5 focus:ring-2 focus:ring-[#2375E0]"
+                  />
+
+                  <button
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute right-3 top-12 text-gray-400 cursor-pointer"
+                  >
+                    {showPassword ? <Eye size={20} /> : <EyeOff size={20} />}
+                  </button>
+
+                  <div className="text-right mt-1">
+                    <Link
+                      href="/forgot-password"
+                      className="text-[#2375E0] text-base hover:underline"
+                    >
+                      Forgot password?
+                    </Link>
+                  </div>
+                </div>
+
+                <button
+                  type="submit"
+                  disabled={loading}
+                  className="w-full bg-[#2375E0] text-white py-2.5 rounded-lg font-semibold hover:scale-[1.03] duration-300 disabled:opacity-50 cursor-pointer"
+                >
+                  {loading ? "Logging in..." : "Login"}
+                </button>
+              </form>
+
+              <p className="text-center text-sm text-[#A6A6A6] mt-8">
+                © 2025. All rights reserved.
+              </p>
+            </div>
+            <ToastContainer position="top-right" autoClose={3000} />
+          </div>
         </div>
 
-        <ToastContainer position="top-right" autoClose={3000} />
+        <div className="bg-[#2375E0] rounded-xl md:col-span-2 ">
+          <Image
+            src={"/images/auth/right-bg.png"}
+            height={400}
+            width={490}
+            alt="Auth Background"
+            className="h-full w-full"
+          />
+        </div>
       </div>
-    </AuthLayout>
+    </div>
   );
 }
--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -3,12 +3,14 @@
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import AuthLayout from "@/components/auth/AuthLayout";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Eye, EyeOff } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function SignupPage() {
+  const router = useRouter();
+
   const [formData, setFormData] = useState({
     full_name: "",
     phone: "",
@@ -38,7 +40,7 @@ export default function SignupPage() {
 
     setLoading(true);
     try {
-      const res = await fetch("http://192.168.112.19:3000/api/auth/register", {
+      const res = await fetch(`/api/auth/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
@@ -46,30 +48,23 @@ export default function SignupPage() {
 
       const data = await res.json();
 
-      if (res.ok) {
-        toast.success("Signup successful! Please check your email/OTP.");
-        setFormData({
-          full_name: "",
-          phone: "",
-          company_name: "",
-          email: "",
-          password: "",
-          agree: false,
-        });
-        setTimeout(() => router.push("/dashboard"), 1000);
-      } else {
-        toast.error(data.message || "Signup failed. Try again.");
+      if (!res.ok) {
+        toast.error(data.message || "Signup failed!");
+        return;
       }
+
+      toast.success("Signup successful!");
+      setTimeout(() => router.push("/login"), 1000);
     } catch (error) {
-      toast.error("Network error. Please try again later.");
+      toast.error("Network error! Try again.");
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <AuthLayout>
-      <div className="bg-white rounded-2xl p-6">
+    <div className="min-h-screen grid grid-cols-1 md:grid-cols-5 bg-[#E9EFF7] gap-8 p-6">
+      <div className="p-8 bg-white rounded-xl md:col-span-3">
         <div className="flex justify-between items-center mb-6">
           <Image
             src="/images/auth/nest-logo.png"
@@ -78,7 +73,7 @@ export default function SignupPage() {
             height={50}
             className="object-contain"
           />
-          <p className="font-[500] text-base leading-6 text-black">
+          <p className="font-medium text-base leading-6 text-black">
             Already have an account?{" "}
             <Link href="/login" className="text-[#2375E0] font-bold">
               Sign In
@@ -87,179 +82,128 @@ export default function SignupPage() {
         </div>
 
         <div className="px-8 pt-8">
-          <h2 className="font-[700] text-[40px] text-black mb-4">
+          <h2 className="font-bold text-[40px] text-black mb-4">
             Sign up to get started
           </h2>
 
           <form onSubmit={handleSubmit} className="space-y-4">
             <div className="flex flex-col gap-2">
-              <label
-                htmlFor="full_name"
-                className="font-medium text-lg leading-5 text-black"
-              >
+              <label className="font-medium text-lg text-black">
                 Full Name
               </label>
               <input
                 type="text"
-                id="full_name"
                 name="full_name"
                 placeholder="Enter your full name"
                 value={formData.full_name}
                 onChange={handleChange}
                 required
-                className="w-full border border-[#A6A6A6] rounded-lg p-2.5 focus:outline-none focus:ring-2 focus:ring-[#2375E0] font-medium text-base leading-6 text-[#A6A6A6]"
+                className="w-full border border-[#A6A6A6] rounded-lg p-2.5 focus:ring-2 focus:ring-[#2375E0]"
               />
             </div>
 
             <div className="grid grid-cols-2 gap-4">
               <div className="flex flex-col gap-2">
-                <label
-                  htmlFor="phone"
-                  className="font-medium text-lg leading-5 text-black"
-                >
+                <label className="font-medium text-lg text-black">
                   Mobile Number
                 </label>
                 <input
                   type="text"
-                  id="phone"
                   name="phone"
                   placeholder="Enter your mobile number"
                   value={formData.phone}
                   onChange={handleChange}
                   required
-                  className="w-full border border-[#A6A6A6] rounded-lg p-2.5 focus:outline-none focus:ring-2 focus:ring-[#2375E0] font-medium text-base leading-6 text-[#A6A6A6]"
+                  className="w-full border border-[#A6A6A6] rounded-lg p-2.5 focus:ring-2 focus:ring-[#2375E0]"
                 />
               </div>
 
               <div className="flex flex-col gap-2">
-                <label
-                  htmlFor="company_name"
-                  className="font-medium text-lg leading-5 text-black"
-                >
+                <label className="font-medium text-lg text-black">
                   Company Name
                 </label>
                 <input
                   type="text"
-                  id="company_name"
                   name="company_name"
-                  placeholder="Enter your company name"
+                  placeholder="Enter company name (optional)"
                   value={formData.company_name}
                   onChange={handleChange}
-                  className="w-full border border-[#A6A6A6] rounded-lg p-2.5 focus:outline-none focus:ring-2 focus:ring-[#2375E0] font-medium text-base leading-6 text-[#A6A6A6]"
+                  className="w-full border border-[#A6A6A6] rounded-lg p-2.5 focus:ring-2 focus:ring-[#2375E0]"
                 />
               </div>
             </div>
 
             <div className="flex flex-col gap-2">
-              <label
-                htmlFor="email"
-                className="font-medium text-lg leading-5 text-black"
-              >
-                Email
-              </label>
+              <label className="font-medium text-lg text-black">Email</label>
               <input
                 type="email"
-                id="email"
                 name="email"
                 placeholder="Enter your email"
                 value={formData.email}
                 onChange={handleChange}
                 required
-                className="w-full border border-[#A6A6A6] rounded-lg p-2.5 focus:outline-none focus:ring-2 focus:ring-[#2375E0] font-medium text-base leading-6 text-[#A6A6A6]"
+                className="w-full border border-[#A6A6A6] rounded-lg p-2.5 focus:ring-2 focus:ring-[#2375E0]"
               />
             </div>
 
             <div className="flex flex-col gap-2 relative">
-              <label
-                htmlFor="password"
-                className="font-medium text-lg leading-5 text-black"
-              >
-                Password
-              </label>
+              <label className="font-medium text-lg text-black">Password</label>
               <input
                 type={showPassword ? "text" : "password"}
-                id="password"
                 name="password"
-                placeholder="Enter your password"
+                placeholder="Enter password"
                 value={formData.password}
                 onChange={handleChange}
                 required
-                className="w-full border border-[#A6A6A6] rounded-lg p-2.5 focus:outline-none focus:ring-2 focus:ring-[#2375E0]"
+                className="w-full border border-[#A6A6A6] rounded-lg p-2.5 focus:ring-2 focus:ring-[#2375E0]"
               />
               <button
                 type="button"
                 onClick={() => setShowPassword(!showPassword)}
-                className="absolute right-3 top-10 text-gray-400 cursor-pointer"
+                className="absolute right-3 top-12 text-gray-400"
               >
                 {showPassword ? <Eye size={20} /> : <EyeOff size={20} />}
               </button>
             </div>
 
-            <div className="flex items-center space-x-2">
+            <div className="flex items-center gap-2">
               <input
                 type="checkbox"
-                id="agree"
                 name="agree"
                 checked={formData.agree}
                 onChange={handleChange}
                 className="w-4 h-4 accent-[#2375E0]"
               />
-              <label htmlFor="agree" className="text-sm">
+              <label className="text-sm">
                 I agree to the{" "}
                 <a href="#" className="text-[#2375E0] underline">
-                  terms and conditions
+                  terms & conditions
                 </a>
-                .
               </label>
             </div>
 
             <button
               type="submit"
               disabled={loading}
-              className="w-full bg-[#2375E0] text-white py-2.5 rounded-lg font-semibold hover:scale-[1.03] transition-all duration-300 cursor-pointer"
+              className="w-full bg-[#2375E0] text-white py-2.5 rounded-lg font-semibold hover:scale-[1.03] disabled:opacity-50 transition"
             >
               {loading ? "Signing up..." : "Sign Up"}
             </button>
-
-            <div className="flex items-center my-4">
-              <div className="flex-grow h-px bg-[#A6A6A6]"></div>
-              <span className="px-2 text-sm text-gray-500">or</span>
-              <div className="flex-grow h-px bg-[#A6A6A6]"></div>
-            </div>
-
-            <div className="grid grid-cols-2 gap-4">
-              <button
-                type="button"
-                className="w-full flex items-center justify-center border border-[#A6A6A6] rounded-lg py-2 hover:scale-[1.03] transition-all duration-300 cursor-pointer font-medium text-base leading-6 text-black"
-              >
-                <Image
-                  src="/images/auth/google.png"
-                  alt="Google"
-                  width={20}
-                  height={20}
-                  className="mr-2"
-                />
-                Log in with Google
-              </button>
-              <button
-                type="button"
-                className="w-full flex items-center justify-center border border-[#A6A6A6] rounded-lg py-2 hover:scale-[1.03] transition-all duration-300 cursor-pointer font-medium text-base leading-6 text-black"
-              >
-                <Image
-                  src="/images/auth/facebook.png"
-                  alt="Facebook"
-                  width={20}
-                  height={20}
-                  className="mr-2"
-                />
-                Log in with Facebook
-              </button>
-            </div>
           </form>
         </div>
 
         <ToastContainer position="top-right" autoClose={3000} />
       </div>
-    </AuthLayout>
+
+      <div className="bg-[#2375E0] rounded-xl md:col-span-2">
+        <Image
+          src="/images/auth/right-bg.png"
+          alt="Auth Background"
+          width={490}
+          height={400}
+          className="h-full w-full object-cover"
+        />
+      </div>
+    </div>
   );
 }
