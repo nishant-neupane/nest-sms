fc2de81fb22e5ea00e2a6594fab69bc949698360
nishant-neupane <nishantneupane488@gmail.com>
2025-10-29 10:25:11 +0545

    fix

--- src/components/wrapper/AuthWrapper.jsx
+++ src/components/wrapper/AuthWrapper.jsx
@@ -9,51 +9,51 @@ export default function AuthWrapper({ children }) {
   const [loading, setLoading] = useState(true);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  // useEffect(() => {
-  //   checkAuth();
-  // }, [pathname]);
+  useEffect(() => {
+    checkAuth();
+  }, [pathname]);
 
-  // const checkAuth = async () => {
-  //   try {
-  //     const res = await fetch("api/auth/me", {
-  //       method: "GET",
-  //       credentials: "include",
-  //     });
+  const checkAuth = async () => {
+    try {
+      const res = await fetch("api/auth/me", {
+        method: "GET",
+        credentials: "include",
+      });
 
-  //     if (res.ok) {
-  //       setIsAuthenticated(true);
+      if (res.ok) {
+        setIsAuthenticated(true);
 
-  //       if (pathname === "/login" || pathname === "/signup") {
-  //         router.push("/dashboard");
-  //       }
-  //     } else {
-  //       setIsAuthenticated(false);
+        if (pathname === "/login" || pathname === "/signup") {
+          router.push("/dashboard");
+        }
+      } else {
+        setIsAuthenticated(false);
 
-  //       const publicRoutes = ["/login", "/signup", "/forgot-password"];
-  //       if (!publicRoutes.includes(pathname)) {
-  //         router.push("/login");
-  //       }
-  //     }
-  //   } catch (error) {
-  //     console.error("Auth check failed:", error);
-  //     setIsAuthenticated(false);
+        const publicRoutes = ["/login", "/signup", "/forgot-password"];
+        if (!publicRoutes.includes(pathname)) {
+          router.push("/login");
+        }
+      }
+    } catch (error) {
+      console.error("Auth check failed:", error);
+      setIsAuthenticated(false);
 
-  //     const publicRoutes = ["/login", "/signup", "/forgot-password"];
-  //     if (!publicRoutes.includes(pathname)) {
-  //       router.push("/login");
-  //     }
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
+      const publicRoutes = ["/login", "/signup", "/forgot-password"];
+      if (!publicRoutes.includes(pathname)) {
+        router.push("/login");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
 
-  // if (loading) {
-  //   return (
-  //     <div className="flex items-center justify-center min-h-screen">
-  //       <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-[#2375E0]"></div>
-  //     </div>
-  //   );
-  // }
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-[#2375E0]"></div>
+      </div>
+    );
+  }
 
   return children;
 }
